2612f7f1a210bd30e78a2e6bd4b9c4541e82c345
Nastaran Rakhshan <80073157+Nas26@users.noreply.github.com>
2023-07-29 21:23:58 -0400

    update

---
+++ challenge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# -------------------
+# Author: Nastaran
+# DAte: 02/June/2023
+# Topic: Challenge
+#version: 2.0.1
+# -------------------
+
+for i in {1..25}
+do 
+    touch Nastaran${i}
+ done
+
+#This loop goes through 1 to 25 and creates 25 files named Nastaran. 
+
+touch file{1..25}$counter.sh
